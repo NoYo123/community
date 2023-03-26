02d9137f20a2299d8af247a7033ef8a5655104d4
NoYo123 <1046693853@qq.com>
2023-03-26 15:45:52 +0800

    extra github config from code to properties

---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,51 @@
+package life.majiang.community.provider;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()  //此处有修改，github废弃了明文传输access_token，需要把access_token放进header里面，和网络状况也有关系
+                    //github新版请求方式
+                    .url("https://api.github.com/user")
+                    .header("Authorization","token "+accessToken)
+//               .url("https://api.github.com/user?access_token=" + accessToken)
+                    .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+}
